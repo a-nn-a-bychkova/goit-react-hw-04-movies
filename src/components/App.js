569a79e1844b0e3501b3b5d52172aeb4c5002c8b
Anna Bychkova <bychkova.dev@gmail.com>
2021-01-18 19:05:51 +0200

    initial installments and some components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component, useState } from 'react';
+import AppBar from './AppBar';
+import Container from './Container';
+
+function App() {
+  return (
+    <div>
+      <h1>FInally</h1>
+      {/* <Container>
+        <AppBar />
+      </Container> */}
+    </div>
+  );
+}
+
+export default App;
